fa384370262d2fab431435e4b449eb308dd4fdd7
noraj <alexandre.zanni@europe.com>
2025-11-02 14:35:13 +0100

    test new utils methods

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -4,30 +4,42 @@ require 'minitest/autorun'
 require 'unisec'
 
 class UnisecTest < Minitest::Test
-  def test_unisec_utils_char2codepoint
+  def test_unisec_utils_string_char2codepoint
     assert_equal('U+00E9', Unisec::Utils::String.char2codepoint('é'))
     assert_equal('U+0041', Unisec::Utils::String.char2codepoint('AZ'))
   end
 
-  def test_unisec_utils_chars2codepoints
+  def test_unisec_utils_string_chars2codepoints
     assert_equal('U+00E9', Unisec::Utils::String.chars2codepoints('é'))
     assert_equal('U+0041 U+005A', Unisec::Utils::String.chars2codepoints('AZ'))
   end
 
-  def test_unisec_utils_deccp2stdhexcp
+  def test_unisec_utils_integer_deccp2stdhexcp
     assert_equal('U+1F680', Unisec::Utils::Integer.deccp2stdhexcp(128640))
     assert_equal('U+0020', Unisec::Utils::Integer.deccp2stdhexcp(32))
   end
 
-  def test_unisec_utils_grapheme_reverse
+  def test_unisec_utils_string_grapheme_reverse
     assert_equal('🇫🇷🐓', Unisec::Utils::String.grapheme_reverse('🐓🇫🇷'))
   end
 
-  def test_unisec_utils_convert
+  def test_unisec_utils_string_convert
     assert_equal(128169, Unisec::Utils::String.convert('0x1f4a9', :integer))
     assert_equal(128169, Unisec::Utils::String.convert('0d128169', :integer))
     assert_equal(128169, Unisec::Utils::String.convert('0b11111010010101001', :integer))
     assert_equal(128169, Unisec::Utils::String.convert('💩', :integer))
+  end
+
+  def test_unisec_utils_string_to_range
+    assert_equal(128..255, Unisec::Utils::String::to_range('0080..00FF'))
+  end
+
+  def test_unisec_utils_string_stdhexcp2deccp
+    assert_equal(8230, Unisec::Utils::String.stdhexcp2deccp('U+2026'))
+  end
+
+  def test_unisec_utils_range_range2codepoint_range
+    assert_equal("U+100000 - U+10FFFF", Unisec::Utils::Range.range2codepoint_range(1048576..1114111))
   end
 
   def test_unisec_utils_to_hex
@@ -36,5 +48,14 @@ class UnisecTest < Minitest::Test
 
   def test_unisec_utils_to_bin
     assert_equal('101010', 42.to_bin)
+  end
+
+  def test_unisec_utils_to_bool
+    assert_equal(true, "true".to_bool)
+  end
+
+  def test_unisec_utils_include_range?
+    assert_equal(false, (1..10).include_range?(2..11))
+    assert_equal(true, (1..10).include_range?(2..4))
   end
 end
